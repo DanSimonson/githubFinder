21b4b3742e87d2376bee67596b451ac533589f40
DanSimonson <32283441+DanSimonson@users.noreply.github.com>
2018-10-27 18:41:03 +0800

    updated

--- src/components/Searchme.vue
+++ src/components/Searchme.vue
@@ -1,16 +1,68 @@
 <template>
-  <!--background: url(https://res.cloudinary.com/dmglopmul/image/upload/v1540531394/projectPhotos/githubfinder/github.jpg);background-repeat: no-repeat;background-size: cover;-->
-  <div class="searchme" style="display: flex;justify-content: center;margin: 10px;">
+  <div>
+    <div class='container wrapAll'>
+      <form>
+        <label class="label" for="GitHub Username">GitHub Username</label>
+        <input type="text" id="username" name="githubname" placeholder="DanSimonson" v-model="username">
+        <!--<label class="label purpleLabel" for="fname"><i class="fab fa-github-square fa-3x"></i>Username: </label>
+      <input class="myInput" type="text" placeholder="DanSimonson" v-model="username" />-->
+        <!--<input type="submit" value="Octo~Search" class="button" v-on:click="chainPromises(username)">-->
+        <a class="button" v-on:click="chainPromises(username)"><i class="fab fa-github-square fa-lg"></i>
+          Octo~Search</a>
+      </form>
+    </div>
+    <div class='container wrapContent showMe' v-if='showMe'>
+      <div class="box">
+        <div class="imgBx label
+      purpleLabel">
+          <img v-bind:src="showMe.avatar_url" />
+          <span v-if="showMe.blog"><a class="button" v-bind:href="showMe.blog" target="_blank"><i class="fas fa-desktop"></i>
+              Website</a></span>
+          <br>
+          <span v-if="showMe.html_url"><a class="button" v-bind:href="showMe.html_url" target="_blank"><i class="fas fa-male"></i>
+              Profile</a></span>
+        </div>
+      </div>
+      <div class="contentBox">
+        <a class="contentLabel"> Name: {{ showMe.name }}</a>
+        <!--<div class="contentLabel" v-if="showMe.name"><i class="fas fa-check fa-lg"></i> Name: {{ showMe.name }}</div>-->
+        <a class="contentLabel" v-if="showMe.bio"></i> Bio~ {{ showMe.bio }}</a>
+        <a class="contentLabel" v-if="showMe.location"></i> Living in: {{
+          showMe.location }}</a>
+        <a class="contentLabel" v-if="showMe.followers"> Followers: {{
+          showMe.followers }}</a>
+        <a class="contentLabel" v-if="showMe.following"> Following: {{
+          showMe.following }}</a>
+      </div>
+    </div>
+    <div v-if="showMe" class="arrowDown" v-bind:style="{ opacity: activeOpacity }"></div>
+
+
+
+
+    <!--background: url(https://res.cloudinary.com/dmglopmul/image/upload/v1540531394/projectPhotos/githubfinder/github.jpg);background-repeat: no-repeat;background-size: cover;-->
+    <!--<div class="searchme" style="display: flex;justify-content: center;margin: 10px;">
     <div class='wrap'>
       <form class="
     myForm">
         <label class="label purpleLabel" for="fname"><i class="fab fa-github-square fa-3x"></i>Username: </label>
         <input class="myInput" type="text" placeholder="DanSimonson" v-model="username" />
         <!--<input type="submit" value="Octocat~me" class="button" v-on:click="chainPromises(username)">-->
-        <span><a class="button" v-on:click="chainPromises(username)">Octo~Search</a></span>
+    <!--<span><a class="button" v-on:click="chainPromises(username)">Octo~Search</a></span>
       </form>
     </div>
-    <div class="
+    <div class="container">
+      <div class="
+        box showMe" v-if="showMe">
+        <div class="imgBx label purpleLabel">
+          <img v-bind:src="showMe.avatar_url" />
+          <span v-if="showMe.blog"><a class="button" v-bind:href="showMe.blog" target="_blank">Website</a></span>
+          <br>
+          <span v-if="showMe.html_url"><a class="button" v-bind:href="showMe.html_url" target="_blank">Profile</a></span>
+        </div>
+      </div>
+    </div>
+    <!--<div class="
             box showMe" v-if="showMe">
       <div class="imgBx label purpleLabel">
         <img v-bind:src="showMe.avatar_url" />
@@ -25,34 +77,35 @@
         <div class="label" v-if="showMe.followers">Followers: {{ showMe.followers }}</div>
         <div class="label" v-if="showMe.following">Following: {{ showMe.following }}</div>
       </div>
-    </div>
+    </div>-->
 
-    <div class='wrapContainer' style="display:flex; justify-content:center; background: red;background-size:cover;">
-      <span class="repos label blueLabel" v-if="showMe">Latest Repositories</span>
-      <v-container grid-list-xl>
-        <v-layout align-items-stretch row wrap>
-          <v-flex pl-4 sm12 lg3 v-for="(repo,index) in showRepos" :key="repo.id" v-if="index <= limit">
-            <v-card align-content-end color="#9068be" class="white--text" height="100%">
-              <v-img src="https://res.cloudinary.com/dmglopmul/image/upload/v1540531394/projectPhotos/githubfinder/github.jpg"
-                aspect-ratio="2.75"></v-img>
-              <v-card-title primary-title>
-                <div class="headline">Repository Name: {{ repo.name }}</div>
-              </v-card-title>
-              <v-card-title>
-                <div>Description: {{repo.description}}</div>
-              </v-card-title>
-              <v-card-actions>
-                <v-btn :href="repo.html_url" target="_blank" rounded color="#e62739" class="white--text">View
-                  Repository</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-      <div v-if="showMe" class="arrowDown" v-bind:style="{ opacity: activeOpacity }"></div>
-    </div>
+    <!--<div class='wrapContainer' style="display:flex; justify-content:center; background: red;background-size:cover;">
+      <span class="repos label blueLabel" v-if="showMe">Latest Repositories</span>-->
+    <v-container grid-list-xl>
+      <v-layout align-items-stretch row wrap>
+        <v-flex pl-4 sm12 lg3 v-for="(repo,index) in showRepos" :key="repo.id" v-if="index <= limit">
+          <v-card align-content-end color="#9068be" class="white--text" height="100%">
+            <v-img src="https://res.cloudinary.com/dmglopmul/image/upload/v1540531394/projectPhotos/githubfinder/github.jpg"
+              aspect-ratio="2.75"></v-img>
+            <v-card-title primary-title>
+              <div class="headline">Latest Repo: {{ repo.name }}</div>
+            </v-card-title>
+            <v-card-title>
+              <div>Description: {{repo.description}}</div>
+            </v-card-title>
+            <v-card-actions>
+              <v-btn :href="repo.html_url" target="_blank" rounded color="#e62739" class="white--text">View
+                Repository</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
-
+  </div>
+  <!--<div v-if="showMe" class="arrowDown" v-bind:style="{ opacity: activeOpacity }"></div>
+  </div>
+  </div>-->
 </template>
 
 <script>
@@ -117,14 +170,142 @@
 </script>
 
 <style scoped>
+  .wrapAll {
+    position: relative;
+    /*border: 5px solid red;*/
+    margin-top: 20px;
+    height: 220px;
+    display: flex;
+    justify-content: center;
+    background: #6ed3cf;
+    /*#e1e8f0;*/
+  }
+
+  form.myForm {
+    border: 5px solid black;
+    background: #e1e8f0;
+    padding: 20px;
+    width: 500px;
+    /*text-align: center;
+    position: absolute;
+    left: 50%;
+    top: 45%;
+    transform: translate(-50%, -50%);
+    z-index: 1;
+    font-size: 1.2em;*/
+  }
+
+  input[type=text],
+  select {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 7px solid #9068be;
+    /*#ccc;*/
+    border-radius: 4px;
+    box-sizing: border-box;
+    color: #9068be;
+  }
+
+  /*input[type=text] {
+    padding: 12px 20px;
+    margin: 0 auto;
+    /*margin: 8px 0;
+    box-sizing: border-box;
+    display: block;
+    width: 200px;
+    text-align: center;
+    background: #6ed3cf;
+    /*#e1e8f0;
+    color: #fdfdfd;
+    border-radius: 8%;
+  }*/
+
+
+
+  .label {
+    font-family: Arial;
+    width: 200px;
+    padding: 2px;
+    color: #9068be;
+    font-size: 1.2em;
+    display: block;
+    padding: 2px;
+
+  }
+
+  .blueLabel {
+
+    background-color: #6ed3cf;
+    /*#e7e7e7;*/
+    border-radius: 8%;
+  }
+
+  .purpleLabel {
+    background-color: #9068be;
+    /*#e7e7e7;*/
+    border-radius: 8%;
+  }
+
+  .redLabel {
+    background-color: #e62739;
+    /*#e7e7e7;*/
+    border-radius: 8%;
+
+  }
+
+  /* buttons */
+  .octoBtn {
+    appearance: button;
+    -moz-appearance: button;
+    -webkit-appearance: button;
+    text-decoration: none;
+    font: menu;
+    color: #9068be;
+    /*#fdfdfd;*/
+    background: #6ed3cf;
+    /* ButtonText;*/
+    /*display: inline-block;*/
+    padding: 5px 8px;
+    margin-top: 5px;
+    transition: .3s;
+    font-size: 1.5em;
+
+  }
+
+  .button {
+    appearance: button;
+    -moz-appearance: button;
+    -webkit-appearance: button;
+    text-decoration: none;
+    font: menu;
+    color: #9068be;
+    /*#fdfdfd;*/
+    background: #6ed3cf;
+    /* ButtonText;*/
+    /*display: inline-block;*/
+    padding: 5px 8px;
+    margin-top: 5px;
+    transition: .3s;
+    font-size: 1.5em;
+  }
+
+  .button:hover {
+    background-color: #e62739;
+    color: #6ed3cf;
+    /*#fdfdfd;*/
+    cursor: pointer;
+  }
+
   .arrowDown {
-    /*width: 200px;
+    width: 200px;
     margin: 0 auto;
     text-align: center;
     position: fixed;
-    bottom: 30%;
+    bottom: 10%;
     left: 48%;
-    transform: translate(-50%, -50%);*/
+    transform: translate(-50%, -50%);
   }
 
   .arrowDown::after {
@@ -175,55 +356,8 @@
     font-size: 1.2em !important;
   }
 
-  .label {
-    color: #fdfdfd;
-    padding: 2px;
-    font-family: Arial;
-  }
-
-  .blueLabel {
-
-    background-color: #6ed3cf;
-    /*#e7e7e7;*/
-    border-radius: 8%;
-  }
-
-  .purpleLabel {
-    background-color: #9068be;
-    /*#e7e7e7;*/
-    border-radius: 8%;
-  }
-
-  .redLabel {
-    background-color: #e62739;
-    /*#e7e7e7;*/
-    border-radius: 8%;
-
-  }
-
-  /* Gray */
-  .button {
-    appearance: button;
-    -moz-appearance: button;
-    -webkit-appearance: button;
-    text-decoration: none;
-    font: menu;
-    color: #fdfdfd;
-    ;
-    background: #e62739;
-    /* ButtonText;*/
-    display: inline-block;
-    padding: 2px 8px;
-    margin-top: 5px;
-    transition: .3s
-  }
-
-  .button:hover {
-    background-color: #F06292;
-    color: #fdfdfd;
-  }
-
-  div.searchMe {
+  */
+  /*div.searchMe {
     position: relative;
     background: url(https://res.cloudinary.com/dmglopmul/image/upload/v1540529604/projectPhotos/githubfinder/github.jpg) no-repeat center center fixed;
     -webkit-background-size: cover;
@@ -231,59 +365,34 @@
     -o-background-size: cover;
     background-size: cover;
 
-  }
+  }*/
 
-  form.myForm {
-    /*border: 2px solid red;*/
-    background: #e1e8f0;
-    padding: 3px;
-    width: 500px;
-    text-align: center;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    z-index: 1;
-    /*font-size: 1.2em;*/
-  }
-
-  label {
-    width: 200px;
-    margin: 0 auto;
-    display: block;
-  }
-
-  input[type=text] {
-    padding: 12px 20px;
-    margin: 0 auto;
-    /*margin: 8px 0;*/
-    box-sizing: border-box;
-    display: block;
-    width: 200px;
-    text-align: center;
-    background: #6ed3cf;
-    /*#e1e8f0;*/
-    color: #fdfdfd;
-    border-radius: 8%;
-  }
-
-  .searchMe {
+  /*.searchMe {
     position: relative;
     height: 100vh;
 
-  }
+  }*/
 
-  .box {
-    background: #e1e8f0;
+  .wrapContent {
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    grid-column-gap: 10px;
+    /*border: 5px solid yellow;*/
   }
-
 
   /*.box .imgbx*/
-  .box .imgBx {
-    width: 100px;
-    flex: 0 0 100px;
-    text-align: center;
+  .box {
+    background: #e1e8f0;
+    margin-right: 10px;
     padding: 5px;
+    /*border: 5px solid brown;*/
+  }
+
+  .box .imgBx {
+    width: 300px;
+    flex: 1 1 200px;
+    text-align: center;
+    padding: 10px;
   }
 
   .box .imgBx img {
@@ -302,10 +411,48 @@
     font-size: 22px;
   }
 
-  .content {
-    margin: 0;
-    padding: 10px 0 0;
+  .contentBox {
     text-align: inherit;
+    background: #e1e8f0;
+    /*background: #6ed3cf;*/
+    color: #9068be;
+    padding: 5px;
+    /*border: 5px solid green;*/
+  }
+
+  .contentLabel {
+    /*
+    color: #9068be;
+    background: #6ed3cf;
+    margin-top: 15px;
+    margin-bottom: 15px;
+    font-size: 1.2em;
+    /*display: block;
+    appearance: button;
+    -moz-appearance: button;
+    -webkit-appearance: button;
+    text-decoration: none;
+    width: 50%;*/
+    appearance: button;
+    -moz-appearance: button;
+    -webkit-appearance: button;
+    text-decoration: none;
+    font: menu;
+    color: #9068be;
+    background: #6ed3cf;
+    /*ButtonText;
+    display: inline-block;...*/
+    padding: 20px;
+    margin-top: 15px;
+    margin-bottom: 15px;
+
+    font-size: 1.2em;
+    display: block;
+    width: 60%;
+  }
+
+  .container.grid-list-xl {
+    background-color: #6ed3cf;
   }
 
 
@@ -313,11 +460,8 @@
     max-width: 100px;
   }*/
 
-  .repos {
-    /*position: absolute;
-    top: 100%;
-    Left: 50%;
-    transform: translate(-50%, -50%);*/
+  /*.repos {
+ 
     width: 200px;
     text-align: center;
   }
@@ -325,20 +469,29 @@
   div.container.grid-list-xl {
     background: #e1e8f0;
 
-  }
+  }*/
 
 
 
   /*media Queries*/
   @media only screen and (min-width: 320px) {
-    form.myForm {
+    .wrapContent {
+      display: grid;
+      grid-template-columns: 1fr;
+      /*grid-column-gap: 10px;
+      border: 5px solid yellow;*/
+    }
+
+
+
+    /*form.myForm {
       width: 320px;
       text-align: center;
       position: absolute;
       left: 50%;
       top: 10%;
       transform: translate(-50%, -50%);
-      /*border: 5px solid red;*/
+      /*border: 5px solid red;
       margin-top: 40px;
       padding: 20px;
     }
@@ -383,9 +536,7 @@
     }
 
     .arrowDown {
-      /*width: 200px;
-    margin: 0 auto;
-    text-align: center;*/
+     
       position: fixed;
       bottom: 12%;
       left: 44%;
@@ -401,11 +552,18 @@
       Left: 50%;
       transform: translate(-50%, -10%);
       padding: 20px;
-    }
+    }*/
   }
 
   @media only screen and (min-width: 768px) {
-    form.myForm {
+    .wrapContent {
+      display: grid;
+      grid-template-columns: 1fr 3fr;
+      /*grid-column-gap: 10px;
+      border: 5px solid yellow;*/
+    }
+
+    /*form.myForm {
       width: 320px;
       text-align: center;
       position: absolute;
@@ -455,9 +613,7 @@
     }
 
     .arrowDown {
-      /*width: 200px;
-    margin: 0 auto;
-    text-align: center;*/
+    
       position: fixed;
       bottom: 28%;
       left: 44%;
@@ -473,11 +629,11 @@
       Left: 50%;
       transform: translate(-50%, -10%);
       padding: 20px;
-    }
+    }*/
   }
 
 
-  @media only screen and (min-width: 1024px) {
+  /*@media only screen and (min-width: 1024px) {
     form.myForm {
 
       width: 320px;
@@ -529,9 +685,7 @@
     }
 
     .arrowDown {
-      /*width: 200px;
-    margin: 0 auto;
-    text-align: center;*/
+     
       position: fixed;
       bottom: 27%;
       left: 46%;
@@ -601,9 +755,7 @@
     }
 
     .arrowDown {
-      /*width: 200px;
-    margin: 0 auto;
-    text-align: center;*/
+      
       position: fixed;
       bottom: 24%;
       left: 46%;
@@ -675,9 +827,7 @@
     }
 
     .arrowDown {
-      /*width: 200px;
-    margin: 0 auto;
-    text-align: center;*/
+      
       position: fixed;
       bottom: 18%;
       left: 48%;
@@ -744,9 +894,7 @@
     }
 
     .arrowDown {
-      /*width: 200px;
-    margin: 0 auto;
-    text-align: center;*/
+      
       position: fixed;
       bottom: 34%;
       left: 48%;
@@ -812,9 +960,7 @@
     }
 
     .arrowDown {
-      /*width: 200px;
-    margin: 0 auto;
-    text-align: center;*/
+      
       position: fixed;
       bottom: 47%;
       left: 49%;
@@ -830,5 +976,5 @@
       transform: translate(-50%, -10%);
       padding: 20px;
     }
-  }
+  }*/
 </style>
